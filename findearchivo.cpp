8f4f1e0de5f1401cd1172c9569eef49080daca93
Tati G <jaelgonzales18@gmail.com>
2024-04-23 21:48:38 -0400

    funciona?

---
+++ findearchivo.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+    double costo;
+    while(cin>>costo) {
+        //cout <<costo<<endl;
+        printf("%d\n", costo);
+    }
+    return 0;
+}
